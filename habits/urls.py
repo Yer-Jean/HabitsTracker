7215f8ae3683c28a56682f92a486f723dc5487b4
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2024-02-11 23:29:04 +0600

    CRUD with permissions for Habit realized

--- habits/urls.py
+++ habits/urls.py
@@ -7,6 +7,9 @@ app_name = HabitsConfig.name
 
 
 urlpatterns = [
-    # path('', HabitsListAPIView.as_view(), name='habits_list'),
+    path('', HabitsListAPIView.as_view(), name='habits_list'),
     path('create/', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('<int:pk>/', HabitRetrieveView.as_view(), name='habit_retrieve'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habit_delete'),
 ]
